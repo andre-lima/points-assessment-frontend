c58fdc98618775c3e8b37bdd8503002c48662e4e
Andre Dantas Lima <andre.dantas.lima@gmail.com>
2025-10-27 13:50:22 -0400

    feat(salary-info): Avoiding repetition of YearButton component

--- src/components/SalaryInformationInput/index.tsx
+++ src/components/SalaryInformationInput/index.tsx
@@ -1,4 +1,4 @@
-import { useSalary, useSalaryDispatch } from '../../store/hooks.ts';
+import { useSalaryDispatch } from '../../store/hooks.ts';
 import { Flex, Heading, TextField, Tooltip } from '@radix-ui/themes';
 import type { ChangeEvent, MouseEvent } from 'react';
 import type { SalaryData } from '../../models/SalaryData.ts';
@@ -6,7 +6,6 @@ import { InfoCircledIcon } from '@radix-ui/react-icons';
 import { YearButton } from '../YearButton';
 
 export const SalaryInformationInput = () => {
-  const { year, salary } = useSalary();
   const dispatch = useSalaryDispatch();
 
   const handleSalaryChange = (evt: ChangeEvent<HTMLInputElement>) => {
@@ -21,15 +20,17 @@ export const SalaryInformationInput = () => {
 
   return (
     <Flex mt="4" p="4" gap="4" direction="column">
-      <Heading size="3">Enter your salary and year below:</Heading>
+      <Heading size="3">Enter your salary and taxation year below:</Heading>
+
       <TextField.Root type={'number'} onChange={handleSalaryChange} placeholder="Enter your salary here..." size="3">
         <TextField.Slot>$</TextField.Slot>
       </TextField.Root>
+
       <Flex gap="2" onClick={handleSetYear}>
-        <YearButton year={2019} />
-        <YearButton year={2020} />
-        <YearButton year={2021} />
-        <YearButton year={2022} />
+        {[2019, 2020, 2021, 2022].map((year) => (
+          <YearButton year={year} />
+        ))}
+
         <Tooltip content="This year might not be available for calculations.">
           <YearButton color="amber" year={2023}>
             <InfoCircledIcon style={{ pointerEvents: 'none' }} />
